9e140236eb2784285a8f1273712cc6fd8093192f
ednow <3334300267@qq.com>
2022-04-04 14:27:59 +0800

    leetcode70 ac

--- leetcode70/Solution.java
+++ leetcode70/Solution.java
@@ -1,14 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+    // 记忆之前找到过的数字
+    public Map<Integer, Integer> remember = new HashMap<>();
+
+
     public int climbStairs(int n) {
         if (n == 0) {
             return 1;
         }
 
-        if (n==-1){
+        if (n == -1) {
             return 0;
         }
 
-        return climbStairs(n-2) + climbStairs(n-1);
+        if (!remember.containsKey(n)) {
+            remember.put(n, climbStairs(n - 2) + climbStairs(n - 1));
+        }
+        return remember.get(n);
     }
 
     public static void main(String[] args) {

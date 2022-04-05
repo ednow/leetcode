0beb77a763260ecc150747397657aa188f9fb3d6
ednow <3334300267@qq.com>
2022-04-05 23:56:36 +0800

    leetcode23 bug:[[1,4,5],[1,3,4],[2,6]]

--- leetcode23/Solution.java
+++ leetcode23/Solution.java
@@ -1,5 +1,80 @@
 package leetcode23;
 
+
+/**
+* Definition for singly-linked list.
+* public class ListNode {
+*     int val;
+*     ListNode next;
+*     ListNode() {}
+*     ListNode(int val) { this.val = val; }
+*     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+* }
+*/
+
+
+class ListNode {
+ int val;
+ ListNode next;
+ ListNode() {}
+ ListNode(int val) { this.val = val; }
+ ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+/**
+ * https://leetcode.com/problems/merge-k-sorted-lists/
+ */
 public class Solution {
 
+    ListNode merge2Lists(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return a == null ? b : a;
+        }
+        ListNode head = a.val > b.val ? b : a;
+        // a永远指向最小的节点
+        while (a != null) {
+            if (a.val > b.val) {
+                ListNode temp;
+                temp = a;
+                a = b;
+                b = temp;
+            }
+            // 如果a后面还有别的节点而且
+            // 如果a后面的值一直相等的话，或者比b小那么指针往后移
+            while (a.next!=null && (a.val == a.next.val || a.val <= b.val)) a = a.next;
+
+            // 指针重置
+            ListNode nextA = null;
+            if (a.next != null) {
+                nextA = a.next.next;
+            }
+            a.next = b;
+            a = nextA;
+
+            // 如果b后面还有别的节点而且
+            // 如果b后面的值一直相等的话
+            while (b.next!=null && b.val==b.next.val) b = b.next;
+            
+        }
+
+        return head;
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+
+        if (lists.length == 1) {
+            return lists[0];
+        }
+
+        ListNode head = merge2Lists(lists[0], lists[1]);
+
+        for (int i = 2; i < lists.length; i++) {
+            head = merge2Lists(head, lists[i]);
+        }
+
+        return head;
+    }
 }
---
+++ leetcode23/Test.java
@@ -0,0 +1,35 @@
+package leetcode23;
+
+public class Test {
+    Solution s = new Solution();
+    static Test t = new Test();
+
+    /**
+     * 根据数字数组创建链表
+     * @param arr 节点值数组
+     * @return 创建好链表的头节点
+     */
+    ListNode initList(int [] arr) {
+        var head = new ListNode(arr[0]);
+        var a = head;
+        for (int i = 1; i < arr.length; i++) {
+            a.next = new ListNode(arr[i]);
+            a = a.next;
+        }
+
+        return head;
+    }
+
+    void case1() {
+//        [[1,4,5],[1,3,4],[2,6]]
+        s.mergeKLists(new ListNode[]{
+                initList(new int[]{1, 4, 5}),
+                initList(new int[]{1, 3, 4}),
+                initList(new int[]{2,6})
+        });
+    }
+
+    public static void main(String[] args) {
+        t.case1();
+    }
+}

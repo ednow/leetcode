e4ef408de6b157e50e6c7adcfcd9e616ae5d8be2
ednow <3334300267@qq.com>
2022-04-08 16:58:07 +0800

    背单调栈的揭发

--- leetcode84/Solution.java
+++ leetcode84/Solution.java
@@ -1,34 +1,37 @@
 package leetcode84;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+
 /**
  * https://leetcode.com/problems/largest-rectangle-in-histogram/discuss/28900/Short-and-Clean-O(n)-stack-based-JAVA-solution
  * https://leetcode-cn.com/problems/largest-rectangle-in-histogram/solution/zhu-zhuang-tu-zhong-zui-da-de-ju-xing-by-leetcode-/
  * 在一维数组中对每一个数找到第一个比自己小的元素。这类“在一维数组中找第一个满足某种条件的数”的场景就是典型的单调栈应用场景。
  * 暴力解法
  */
-public class Solution {
+class Solution {
     public int largestRectangleArea(int[] heights) {
-        int result = 0;
-        for (int i = 0; i < heights.length; i++) {
-            int len = 0;
-            // 向左边扩散
-            for (int j = i-1; j >= 0; j--) {
-                if (heights[j] >= heights[i]) {
-                    len++;
-                } else { // 结束扩散
-                    break;
-                }
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(right, n);
+
+        Deque<Integer> mono_stack = new ArrayDeque<Integer>();
+        for (int i = 0; i < n; ++i) {
+            while (!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]) {
+                right[mono_stack.peek()] = i;
+                mono_stack.pop();
             }
-            // 向右边扩散
-            for (int j = i+1; j < heights.length; j++) {
-                if (heights[j] >= heights[i]) {
-                    len++;
-                } else { // 结束扩散
-                    break;
-                }
-            }
-            result = Math.max(result, heights[i] * (len + 1));
+            left[i] = (mono_stack.isEmpty() ? -1 : mono_stack.peek());
+            mono_stack.push(i);
         }
-        return result;
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
     }
 }
---
+++ leetcode85/Solution.java
@@ -0,0 +1,64 @@
+package leetcode85;
+
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+/**
+ * 题目：https://leetcode.com/problems/maximal-rectangle/
+ * 解法：https://blog.csdn.net/qq_17550379/article/details/86237801
+ * 把每一行以上的区域看成是问题84的直方图
+ */
+public class Solution {
+
+    /**
+     * 背
+     * @param heights
+     * @return
+     */
+    int maximalRectangle(int[] heights) {
+        int ans = 0;
+        int []right = new int[heights.length];
+        int []left = new int[heights.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < heights.length; i++) {
+            while (!stack.isEmpty() && stack.peek() >= heights[i]) {
+                right[stack.pop()] = i;
+            }
+            left[i] = stack.isEmpty() ? -1: stack.peek();
+            stack.push(i);
+        }
+
+        for (int i = 0; i < heights.length; i++) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+
+        return ans;
+    }
+
+    public int maximalRectangle(char[][] matrix) {
+        // 初始化高度数组
+        int[][] heights = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            Arrays.fill(heights[i], 0);
+        }
+        for (int i = 0; i < matrix.length; i++) {  // row
+            for (int j = 0; j < matrix[i].length; j++) { // col
+                for (int k = i; k >= 0; k--) {
+                    if (matrix[i][j] == '0') {
+                        break;
+                    }
+                    heights[i][j]++;
+                }
+            }
+        }
+
+        int ans = 0;
+        // 每一行都进行单调栈搜索
+        for (int[] height : heights) {
+            ans = Math.max(ans, maximalRectangle(height));
+        }
+        return ans;
+    }
+}

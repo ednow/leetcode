7c9d6b058689c70683b1617f5ce701514571e940
ednow <3334300267@qq.com>
2022-04-09 15:26:38 +0800

    leetcode547 ac

--- leetcode547/Solution.java
+++ leetcode547/Solution.java
@@ -1,6 +1,5 @@
 package leetcode547;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,35 +7,36 @@ import java.util.Set;
  * https://leetcode-cn.com/problems/number-of-provinces/
  */
 public class Solution {
-    int find(int[] set, int x) {
-        if (set[x] == x) return x;
+    int find(int[] p, int x) {
+        if (p[x] == x) return x;
         // 路径压缩
-        set[x] = find(set, x);
-        return set[x];
+        p[x] = find(p, p[x]);
+        return p[x];
+
     }
 
-    void union(int[] set, int a, int b) {
-        set[find(set, b)] = find(set, a);
+    void union(int[] p, int a, int b) {
+        p[find(p, b)] = find(p, a);
 
     }
 
     public int findCircleNum(int[][] isConnected) {
-        int[] set = new int[isConnected.length];
+        int[] p = new int[isConnected.length];
         for (int i = 0; i < isConnected.length; i++) {
-            set[i] = i;
+            p[i] = i;
         }
 
         for (int i = 0; i < isConnected.length; i++) {
             for (int j = i+1; j < isConnected.length; j++) {
                 if (isConnected[i][j] == 1) {
-                    union(set, i, j);
+                    union(p, i, j);
                 }
             }
         }
 
         Set<Integer> parent = new HashSet<>();
-        for (int i = 0; i < set.length; i++) {
-            parent.add(find(set, i));
+        for (int i = 0; i < p.length; i++) {
+            parent.add(find(p, i));
         }
 
         return parent.size();

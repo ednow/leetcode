e79888d88493b4c34eb8c948e7f3573115bfc909
ednow <3334300267@qq.com>
2022-04-09 18:04:36 +0800

    leetcode209 await {5,1,3,5,10,7,4,9,2,8}

--- leetcode209/Solution.java
+++ leetcode209/Solution.java
@@ -4,19 +4,21 @@ public class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int len = 0;
         int sum = 0;
+        int result = len;
         for (int i = 0; i < nums.length; i++) {
-            if (sum < target && i + len < nums.length) {
+            // 增加右边的范围
+            if (sum < target) {
                 len++;
                 sum = sum + nums[i];
-                continue;
             }
-            if (sum >= target) {
+            // 缩小左边的范围
+            while (sum >= target) {
+                result = len;
                 len--;
                 sum = sum - nums[i - len];
             }
 
         }
-
-        return len;
+        return result;
     }
 }
--- leetcode209/Test.java
+++ leetcode209/Test.java
@@ -2,6 +2,7 @@ package leetcode209;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(new Solution().minSubArrayLen(7, new int[]{2,3,1,2,4,3}));
+//        System.out.println(new Solution().minSubArrayLen(7, new int[]{2,3,1,2,4,3}));
+        System.out.println(new Solution().minSubArrayLen(7, new int[]{5,1,3,5,10,7,4,9,2,8}));
     }
 }

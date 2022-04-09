3e8122743f0d35ce997bc7249431133b45e00aff
ednow <3334300267@qq.com>
2022-04-09 18:12:19 +0800

    leetcode209 ac 执行用时：

--- leetcode209/Test.java
+++ leetcode209/Test.java
@@ -3,6 +3,6 @@ package leetcode209;
 public class Test {
     public static void main(String[] args) {
 //        System.out.println(new Solution().minSubArrayLen(7, new int[]{2,3,1,2,4,3}));
-        System.out.println(new Solution().minSubArrayLen(7, new int[]{5,1,3,5,10,7,4,9,2,8}));
+        System.out.println(new Solution().minSubArrayLen(15, new int[]{5,1,3,5,10,7,4,9,2,8}));
     }
 }

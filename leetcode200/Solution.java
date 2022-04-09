f80af70cd6d546761acf19a4ea971a219015b80d
ednow <3334300267@qq.com>
2022-04-09 16:19:20 +0800

    leetcode200 ac

--- leetcode200/Solution.java
+++ leetcode200/Solution.java
@@ -3,6 +3,9 @@ package leetcode200;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * https://leetcode-cn.com/problems/number-of-islands/
+ */
 public class Solution {
     int find(int[] p, int x) {
         if (p[x]==x) return x;
@@ -15,20 +18,20 @@ public class Solution {
     }
 
     public int numIslands(char[][] grid) {
-        int[] p = new int[grid.length* grid.length];
+        int[] p = new int[grid.length* grid[0].length];
         // 探照方向
-        int[][] pos = new int[][]{{0, 1}, {1, 0}, {1, 1}, {0, -1}, {-1, 0}, {-1, -1}, {-1, 1}, {1, -1}};
+        int[][] pos = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         for (int i = 0; i < p.length; i++) {
             p[i] = i;
         }
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 for (int [] once: pos) {
                     // 如果探照合法而且是连接的
                     int x = i + once[0];
-                    int y = i + once[1];
-                    if (x >= 0 && x < grid.length && y >= 0 && y < grid.length && grid[i][j] == '1' && grid[x][y] == '1') {
-                        union(p, i*j, x*y);
+                    int y = j + once[1];
+                    if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[i][j] == '1' && grid[x][y] == '1') {
+                        union(p, i* grid[0].length + j, x* grid[0].length + y);
                     }
                 }
             }
@@ -36,9 +39,9 @@ public class Solution {
 
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
-                    set.add(find(p, i * j));
+                    set.add(find(p, i* grid[0].length + j));
                 }
             }
         }
--- leetcode200/Test.java
+++ leetcode200/Test.java
@@ -2,8 +2,11 @@ package leetcode200;
 
 public class Test {
     public static void main(String[] args) {
+//        System.out.println(new Solution().numIslands(new char[][]
+//                {{'1','1','0','0','0'},{'1','1','0','0','0'},{'0','0','1','0','0'},{'0','0','0','1','1'}}
+//        ));
         System.out.println(new Solution().numIslands(new char[][]
-                {{'1','1','0','0','0'},{'1','1','0','0','0'},{'0','0','1','0','0'},{'0','0','0','1','1'}}
+                {{'1'},{'1'}}
         ));
     }
 }
